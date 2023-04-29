c3f0828de5cc0dcf6f9f7680d6beef259a66412f
NetesaRoman <netesroman8@gmail.com>
2023-04-29 14:15:50 +0300

    sort fixed + report controller segregation

--- app/controllers/bibliotekas_controller.rb
+++ app/controllers/bibliotekas_controller.rb
@@ -2,33 +2,63 @@ require 'kaminari'
 
 class BibliotekasController < ApplicationController
 
-
   before_action :authenticate_admin!
 
   def index
 
-
+    @up = false
+    @up_genre = false
     @q = Biblioteka.ransack(params[:q])
     @bibliotekas = @q.result(distinct: true).includes(:books)
 
-
     @bibliotekas = @bibliotekas.order(:name) if params[:sort] == "name"
-    @bibliotekas = @bibliotekas.sort_by { |biblioteka| biblioteka.books.count } if params[:sort] == "book_count"
-    @bibliotekas = @bibliotekas.sort_by { |biblioteka| biblioteka.books.select(:genre_id).distinct.count } if params[:sort] == "genre_count"
+    if params[:sort] == "book_count"
+      @bibliotekas = @bibliotekas.sort_by { |biblioteka| biblioteka.books.count }
+      @up = false
+    end
+
+    if params[:sort] == "book_count_desc"
+      @bibliotekas = @bibliotekas.sort_by{ |biblioteka| biblioteka.books.count}.reverse
+      @up = true
+    end
+
+    if params[:sort] == "genre_count"
+      @bibliotekas = @bibliotekas.sort_by { |biblioteka| biblioteka.books.select(:genre_id).distinct.count }
+      @up_genre = false
+    end
+    if params[:sort] == "genre_count_desc"
+      @bibliotekas = @bibliotekas.sort_by { |biblioteka| biblioteka.books.select(:genre_id).distinct.count }.reverse
+      @up_genre = true
+    end
+
     @bibliotekas = Kaminari.paginate_array(@bibliotekas).page(params[:page]).per(10)
   end
 
-
-
   def show
+    @up_age = false
+    @up_card = false
     @biblioteka = Biblioteka.find(params[:id])
-    @users = User.joins(:reader_card).where(reader_cards: {biblioteka_id: @biblioteka.id})
+    @users = User.joins(:reader_card).where(reader_cards: { biblioteka_id: @biblioteka.id })
     @users = @users.where("name LIKE ?", "%#{params[:name]}%") if params[:name].present?
     @users = @users.where(age: params[:age]) if params[:age].present?
 
     @users = @users.order(:name) if params[:sort] == "name"
-    @users = @users.sort_by { |user| user.age } if params[:sort] == "age"
-    @users = @users.sort_by { |user| user.reader_card.id} if params[:sort] == "card"
+    if params[:sort] == "age"
+      @users = @users.sort_by { |user| user.age }
+      @up_age = false
+    end
+    if params[:sort] == "age_desc"
+      @users = @users.sort_by { |user| user.age }.reverse
+      @up_age = true
+    end
+    if params[:sort] == "card"
+      @users = @users.sort_by { |user| user.reader_card.id }
+      @up_card = false
+    end
+    if params[:sort] == "card_desc"
+      @users = @users.sort_by { |user| user.reader_card.id }.reverse
+      @up_card = true
+    end
 
 
     @users = Kaminari.paginate_array(@users).page(params[:page]).per(10)
@@ -64,6 +94,7 @@ class BibliotekasController < ApplicationController
   end
 
   private
+
   def biblioteka_params
     params.require(:biblioteka).permit(:name)
   end
--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,16 +5,20 @@ class ReportController < ApplicationController
 
   def generate_pdf
     @bibliotekas = Biblioteka.all
-
-    kit = PDFKit.new(assemble_report)
+    @users = User.all
+    @biblioteka = Biblioteka.first
+    report_service = ReportService.new(@bibliotekas, @users, @biblioteka)
+    kit = PDFKit.new(report_service.assemble_report)
     kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/report.css"
     send_data(kit.to_pdf, filename: "Libraries Report.pdf", type: 'application/pdf')
   end
 
   def generate_users_pdf
+    @bibliotekas = Biblioteka.all
     @users = User.all
-
-    kit = PDFKit.new(assemble_report_users)
+    @biblioteka = Biblioteka.first
+    report_service = ReportService.new(@bibliotekas, @users, @biblioteka)
+    kit = PDFKit.new(report_service.assemble_report_users)
     kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/report.css"
     send_data(kit.to_pdf, filename: "Users Report.pdf", type: 'application/pdf')
   end
@@ -22,8 +26,10 @@ class ReportController < ApplicationController
   def generate_lib_pdf
     @biblioteka = Biblioteka.find(params[:id])
     @users = User.joins(:reader_card).where(reader_cards: { biblioteka_id: @biblioteka.id })
+    @bibliotekas = Biblioteka.all
+    report_service = ReportService.new(@bibliotekas, @users, @biblioteka)
 
-    kit = PDFKit.new(assemble_report_users)
+    kit = PDFKit.new(report_service.assemble_report_users)
     kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/report.css"
     send_data(kit.to_pdf, filename: "Library Report.pdf", type: 'application/pdf')
   end
@@ -41,80 +47,6 @@ class ReportController < ApplicationController
     end
   end
 
-  private
-
-  def assemble_report
-    report = "<table id = 'modelsTable'> <thead id = 'modelsThead'>"
-    report += "<tr><th > <p>Library Name  </p></th>"
-    report += "<th > <p>Number of Books  </p></th>"
-    report += "<th > <p>Number of Genres  </p></th>"
-    report += "<th> <p>Users</p></th>"
-    @bibliotekas.each do |biblioteka|
-      @users = User.joins(:reader_card).where(reader_cards: { biblioteka_id: biblioteka.id })
-      report += "</tr></thead> <tbody><tr>"
-      report += "<td>" + biblioteka.name + "</td>"
-      report += "<td>" + biblioteka.books.count.to_s + "</td>"
-      report += "<td>" + biblioteka.books.select(:genre_id).distinct.count.to_s + "</td>"
-      report += "<td><ul>"
-      @users.each do |user|
-        report += "<li>" + user.name + "</li>"
-      end
-    end
-    report += "</table>"
-
-    return report
-  end
-
-  def assemble_report_users
-
-    report = "<div style='  display: flex; width: 1000px;'>
-    <div style='margin-right: 1200px; margin-bottom: 100px'>
-      <h2>Name:</h2>
-      <p>"+ @biblioteka.name + "</p>
-      <h2>Address:</h2>
-      <p>"+ @biblioteka.address.to_s + "</p>
-      <h2>Phone:</h2>
-      <p>"+ @biblioteka.phone.to_s + "</p>
-    </div>
 
 
-  </div>
-"
-
-
-    report += "<table id = 'modelsTable'> <thead id = 'modelsThead'>"
-    report += "<tr><th > <p>User Name  </p></th>"
-    report += "<th > <p>User Age  </p></th>"
-    report += "<th > <p>Reader card  </p></th>"
-    @users.each do |user|
-
-      report += "</tr></thead> <tbody><tr>"
-      report += "<td>" + user.name + "</td>"
-      report += "<td>" + user.age.to_s + "</td>"
-      report += "<td>" + user.reader_card.to_s + "</td>"
-
-    end
-    report += "</table>"
-
-    return report
-  end
-
-
-  def assemble_report_lib
-    report = "<table id = 'modelsTable'> <thead id = 'modelsThead'>"
-    report += "<tr><th > <p>User Name  </p></th>"
-    report += "<th > <p>User Age  </p></th>"
-    report += "<th > <p>Reader card  </p></th>"
-    @users.each do |user|
-
-      report += "</tr></thead> <tbody><tr>"
-      report += "<td>" + user.name + "</td>"
-      report += "<td>" + user.age.to_s + "</td>"
-      report += "<td>" + user.reader_card.to_s + "</td>"
-
-    end
-    report += "</table>"
-
-    return report
-  end
 end
