cb67cb850e4089805dcdb0860bfe4231be9b093a
NetesaRoman <netesroman8@gmail.com>
2023-04-30 13:13:39 +0300

    Rake task + multithreading

--- app/models/biblioteka.rb
+++ app/models/biblioteka.rb
@@ -12,39 +12,6 @@ class Biblioteka < ApplicationRecord
   end
 
 
-  def parse_file
-    require 'csv'
-    file_path = File.join(File.dirname(__FILE__), '..', 'assets', 'libraries.csv')
-    table = CSV.parse(File.read(file_path), headers: true)
-
-    100.times do |i|
-      Biblioteka.create(
-        name: table[i][3],
-        address: table[i][4],
-        city: table[i][5],
-        zip_code: table[i][6]
-      )
-    end
-
-  end
-
-  def parse_wiki
-    require 'nokogiri'
-    require 'open-uri'
-
-    doc = Nokogiri::HTML(URI.open('https://en.wikipedia.org/wiki/List_of_libraries'))
-    list_items = doc.css('li')
-
-
-    100.times do |i|
-      Biblioteka.create(
-        name: list_items[i].at_css('a').text,
-        address: list_items[i].text.gsub(list_items[i].at_css('a').text, '').gsub(',', '').strip,
-
-      )
-    end
-
-  end
 
   def self.to_csv
     attributes = %w{name book_count genre_count user_names}
